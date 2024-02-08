7e71f40e90806ea3554cc2c03604d9719f0e7030
Northword <northword@outlook.com>
2024-02-08 17:57:03 +0800

    chore: update deps

--- zotero-plugin.config.ts
+++ zotero-plugin.config.ts
@@ -3,7 +3,18 @@ import { defineConfig } from "zotero-plugin-scaffold";
 export default defineConfig({
   source: ["src", "addon"],
   dist: "build",
-  assets: ["addon/**/*.*", "src/**/*.*", "!src/**/*.ts"],
+  assets: ["addon/**/*.*"],
+  esbuildOptions: [
+    {
+      entryPoints: ["src/index.ts"],
+      define: {
+        __env__: `"${process.env.NODE_ENV}"`,
+      },
+      bundle: true,
+      target: "firefox102",
+      outfile: "build/addon/chrome/content/scripts/addontemplate.js",
+    },
+  ],
 
   extraBuilder: (options) => {
     console.log(JSON.stringify(options, null, 2));
